ea3af4bc38bec2a476a595c3a4818e258279e365
carlaQc <quenallatacarla@gmail.com>
2020-05-01 20:56:34 -0400

    modificacion de diseño

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,19 +1,21 @@
 $(function () {
 
-    // socket.io client side connection
+  // conexión del lado del cliente socket.io
     const socket = io.connect();
     
-    // obtaining DOM elements from the Chat Interface
+    
+// obteniendo elementos DOM de la interfaz de chat
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
-    // obtaining DOM elements from the NicknameForm Interface
+    // obteniendo elementos DOM de la interfaz NicknameForm
     const $nickForm = $('#nickForm');
     const $nickError = $('#nickError');
     const $nickname = $('#nickname');
 
-    // obtaining the usernames container DOM
+    
+    // obtener el DOM del contenedor de nombres de usuario
     const $users = $('#usernames');
 
     $nickForm.submit(e => {
@@ -34,7 +36,7 @@ $(function () {
       $nickname.val('');
     });
 
-    // events
+    // eventos
     $messageForm.submit( e => {
       e.preventDefault();
       socket.emit('send message', $messageBox.val(), data => {
